a65d14f84f1ed9fd25bea6066cf4dc997aa6c087
TJHackers <tjhackers@ibm>
2017-03-26 13:25:34 +0000

    Funfa!

--- test.js
+++ test.js
@@ -1,92 +1,72 @@
-/**
- * Copyright 2016 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-var TJBot = require('tjbot');
-var config = require('./config');
-var request = require("request");
+var TJBot = require('tjbot'),
+    config = require('./config'),
+    request = require('request');
 
 // obtain our credentials from config.js
 var credentials = config.credentials;
 
 // obtain user-specific config
-var WORKSPACEID = config.conversationWorkspaceId;
+const WORKSPACEID = config.conversationWorkspaceId;
+
+// Base Api URL
+const BASEURL = "https://tjhackers-rednode.mybluemix.net/";
 
 // these are the hardware capabilities that TJ needs for this recipe
-var hardware = ['led', 'microphone', 'speaker'];
+var hardware = ['microphone', 'speaker'];
 
 // turn on debug logging to the console
-var tjConfig = {
-    verboseLogging: true
+var configuration = {
+    verboseLogging: true,
+    listen: {
+        language: 'pt-BR'
+    },
+    speak: {
+        language: 'pt-BR'
+    },
+    voice: 'pt-BR_IsabelaVoice'
 };
 
 // instantiate our TJBot!
-var tj = new TJBot(hardware, tjConfig, credentials);
+var tj = new TJBot(hardware, configuration, credentials);
+var questions = [
+  [
+    "question1",
+    "Diga o nome de 3 animais marinhos."
+  ],
+  [
+    "question2",
+    "Diga o nome de 3 planetas do sistema solar."
+  ],
+  [
+    "question3",
+    "Descreva uma ave."
+  ]
+];
 
 
-// full list of colors that TJ recognizes, e.g. ['red', 'green', 'blue']
-var tjColors = tj.shineColors();
 
-
-// listen for utterances with our attentionWord and send the result to
-// the Conversation service
-tj.speak("Diga o nome de 3 animais marinhos.");
-console.log("Diga o nome de 3 animais marinhos.");
-
-tj.sleep(3000);
-console.log("Aguardando resposta...");
+var next = true;
+// listen for speech
 tj.listen(function(msg) {
-//    console.log(msg);
-    console.log("http://tjhackers-rednode.mybluemix.net/question1?msg=" + msg);
-    // send to the conversation service
-    request
-        .get('http://tjhackers-rednode.mybluemix.net/question1?msg=' + msg)
-        .on('response', function(response) {
-            console.log(response.statusCode) // 200
-            console.log(response.headers['content-type']) // 'image/png'
-        })
-});
+    var containsTurn = msg.indexOf("turn") >= 0;
+    var containsChange = msg.indexOf("change") >= 0;
+    var containsSet = msg.indexOf("set") >= 0;
+    var containsLight = msg.indexOf("the light") >= 0;
+    var containsDisco = msg.indexOf("disco") >= 0;
 
-
-tj.sleep(5000);
-tj.speak("Diga o nome de 3 planetas do sistema solar.");
-console.log("Diga o nome de 3 planetas do sistema solar.");
-
-tj.sleep(4000);
-console.log("Aguardando resposta...");
-tj.listen(function(msg) {    
-    console.log(msg);
-    // send to the conversation service
-    request("http://tjhackers-rednode.mybluemix.net/question2?msg=" + msg, function (error, response, body) {
-        console.log(response);
-        tj.speak(response);
-    });
-});
-
-tj.sleep(5000);
-tj.speak("Descreva uma ave.");
-console.log("Descreva uma ave.");
-
-tj.sleep(2000);
-console.log("Aguardando resposta...");
-tj.listen(function(msg) {
-    console.log(msg);
-    // send to the conversation service
-    request("http://tjhackers-rednode.mybluemix.net/question3?msg=" + msg, function (error, response, body) {
-        console.log(response);
-        tj.speak(response);
-    });
+    if ((containsTurn || containsChange || containsSet) && containsLight) {
+        // was there a color uttered?
+        var words = msg.split(" ");
+        for (var i = 0; i < words.length; i++) {
+            var word = words[i];
+            if (colors[word] != undefined || word == "on" || word == "off") {
+                // yes!
+                tj.shine(word);
+                break;
+            }
+        }
+    } else if (containsDisco) {
+        // discoParty();
+    }
 });
 
